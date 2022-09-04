8cca63fe19bcf183a5d9b81491435ba965050b8a
Marco Carrazza <carrazzamarco@gmail.com>
2022-09-03 21:23:35 -0300

    Desafío: Sincronizar counter

--- src/components/ItemDetailContainer/ItemDetail.js
+++ src/components/ItemDetailContainer/ItemDetail.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Card, Row, Col, Stack } from 'react-bootstrap';
 import Counter from './Counter';
+import { NavLink } from 'react-router-dom';
 
 function ItemDetail({ item }) {
-  const [qty, setQty] = useState(item.stock>0 ? 1 : 0);
+  const [qty, setQty] = useState(item.stock > 0 ? 1 : 0);
 
   const updateQty = (value) => {
     console.log('apretaste');
-    if(qty===0 && value<0) return
-    if(qty===item.stock && value>0) return
-    setQty(qty+value)
+    if(qty===0 && value < 0) return
+    if(qty===item.stock && value > 0) return
+    setQty(qty + value)
   }
 
   return ( 
@@ -24,7 +25,9 @@ function ItemDetail({ item }) {
           <h2>Precio: {item.price}</h2>
           <h4>{item.stock > 0 ? <>Stock: {item.stock}</> : <>Sin stock</>}</h4>
           <Counter qty={qty} updateQty={updateQty} />
-          <Button variant='primary' style={{width: 220}} disabled={qty===0 && true}>Agregar al carrito</Button>
+          <Button variant='primary' style={{width: 220}} disabled={qty===0 && true}>
+            <NavLink to={'/cart'} style={{color: 'white', textDecoration: 'none'}}>Terminar compra</NavLink>
+          </Button>
         </Stack>
       </Col>
     </Row>
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ItemListContainer from '../components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from '../components/ItemDetailContainer/ItemDetailContainer';
+import Cart from '../components/Cart/Cart';
 import NavBar from '../components/NavBar/NavBar';
 
 function Rutas() {
@@ -12,6 +13,7 @@ function Rutas() {
         <Route path='/' element={<ItemListContainer />} />
         <Route path='/category/:id' element={<ItemListContainer />} />
         <Route path='/item/:id' element={<ItemDetailContainer />} />
+        <Route path='/cart' element={<Cart />} />
       </Routes>
     </BrowserRouter>
    );
