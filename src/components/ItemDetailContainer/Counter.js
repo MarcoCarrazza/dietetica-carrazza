b393a5f4c44da91bacf6d5f7d98fda3332cd1a2f
Marco Carrazza <carrazzamarco@gmail.com>
2022-09-09 00:43:55 -0300

    Correción desafío: Sincronizar counter

--- src/components/ItemDetailContainer/Counter.js
+++ src/components/ItemDetailContainer/Counter.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import { Form, Button, Container } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Form, Button, Container, Stack } from 'react-bootstrap';
 
-function Counter({ qty, updateQty }) {
-  
+function Counter({ onAdd, stock }) {
+  const [qty, setQty] = useState(1)
+
+  const updateQty = (value) => {
+    if(value === -1 && qty <= 1) return
+    if(value === 1 && qty >= stock) return
+    setQty(qty + value)
+  }
+
   return (
-  <Container className='d-flex'>
-    <Button variant='outline-primary' onClick={() => updateQty(-1)}>-</Button>
-    <Form.Control type='number' value={qty} style={{width: 60, margin: '0 10px'}} readOnly />
-    <Button variant='outline-primary' onClick={() => updateQty(1)}>+</Button>
+  <Container>
+    <Stack gap={3}>
+      <div className='d-flex'>
+        <Button variant='outline-primary' onClick={() => updateQty(-1)}>-</Button>
+        <Form.Control type='number' value={qty} style={{width: 60, margin: '0 10px'}} readOnly />
+        <Button variant='outline-primary' onClick={() => updateQty(1)}>+</Button>
+      </div>
+      <div>
+      <Button variant='primary' style={{width: 220}} onClick={() => onAdd(qty)} disabled={stock===0 && true}>
+        Agregar al carrito
+      </Button>
+      </div>
+    </Stack>
   </Container>
   );
 }
--- src/components/ItemDetailContainer/ItemDetail.js
+++ src/components/ItemDetailContainer/ItemDetail.js
@@ -7,25 +7,24 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function ItemDetail({ item }) {
-  const { addItem } = useContext(CartContext)
-  const [qty, setQty] = useState(item.stock > 0 ? 1 : 0);
+  // const { addItem } = useContext(CartContext)
 
-  const updateQty = (value) => {
-    console.log('apretaste');
-    if(qty===0 && value < 0) return
-    if(qty===item.stock && value > 0) return
-    setQty(qty + value)
+  const [show, setShow] = useState(false);
+  
+  const onAdd = (qty) => {
+    //Acá en teoría debería agregar una acción para agregar al carrito el objeto {...item, qty: qty}, pero ésto escapa al alcance de este desafío, ya que dicha acción se reemplaza por una función addToCart() que venga de un context
+    setShow(!show)
   }
 
-  const notify = () => toast('Producto agregado al carrito!', {
-    position: "top-right",
-    autoClose: 2000,
-    hideProgressBar: true,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    style: {background: 'green', color: 'white'}
-});
+//   const notify = () => toast('Producto agregado al carrito!', {
+//     position: "top-right",
+//     autoClose: 2000,
+//     hideProgressBar: true,
+//     closeOnClick: true,
+//     pauseOnHover: true,
+//     draggable: true,
+//     style: {background: 'green', color: 'white'}
+// });
 
   return ( 
     <>
@@ -38,18 +37,16 @@ function ItemDetail({ item }) {
             <h2>{item.title}</h2>
             <h3>{item.description}</h3>
             <h2>Precio: {item.price}</h2>
-            <h4>{item.stock > 0 ? <>Stock: {item.stock}</> : <>Sin stock</>}</h4>
-            <Counter qty={qty} updateQty={updateQty} />
-            <Button variant='primary' style={{width: 220}} onClick={() => {addItem({...item, qty: qty}); notify()}} disabled={qty===0 && true}>
-              Agregar al carrito
-            </Button>
-            <Button variant='secondary' style={{width: 220}}>
-              <NavLink to={'/cart'} style={{color: 'white', textDecoration: 'none'}}>Ir al carrito</NavLink>
-            </Button>
+            <h4>{item.stock > 0 ? <>Stock disponible: {item.stock}</> : <>Sin stock</>}</h4>
+            {show
+              ? <Button variant='success'>
+                  <NavLink to={'/cart'} style={{color: 'white', textDecoration: 'none'}}>Ir al carrito</NavLink>
+                </Button>
+              : <Counter onAdd={onAdd} stock={item.stock} />}
           </Stack>
         </Col>
       </Row>
-      <ToastContainer />
+      {/* <ToastContainer /> */}
     </>
    );
 }
